8d663917561c3a16e345bf644d0edccbf592c0fa
edenirpm <edenirpm@gmail.com>
2018-03-10 16:27:28 -0300

    Alterado Efeito

--- model/model.EfectTextUP.pas
+++ model/model.EfectTextUP.pas
@@ -2,7 +2,7 @@ unit model.EfectTextUP;
 
 interface
 uses
-Fmx.Objects, FMX.Edit, FMX.Ani,UInterfaces;
+Fmx.Objects, FMX.Edit, FMX.Ani,UInterfaces, System.UITypes;
 type
 TEfectTextUP = class(TInterfacedObject,IEfect)
   procedure EfectTextEdit(AObject:TObject);
@@ -15,6 +15,8 @@ implementation
 procedure TEfectTextUP.EfectTextEdit(AObject: TObject);
 begin
 TAnimator.AnimateFloat((AObject as TText), 'Position.Y', -25,0.60);
+(AObject as TText).TextSettings.Font.Size:= 11;
+(AObject as TText).TextSettings.FontColor:= TAlphaColorRec.Blue;
 //(AObject as TText).AnimateFloat('Position.Y',-20,0.30);IsDeprecated
 ((AObject as TText).Parent as TEdit).SetFocus;
 end;
